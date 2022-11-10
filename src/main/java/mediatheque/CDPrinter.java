621dcaa6f1ba70e8c523dcd46a150d63ad5cd053
JayRay5 <rayanejaybencharef@gmail.com>
2022-11-10 11:59:26 +0100

    finit

--- src/main/java/mediatheque/CDPrinter.java
+++ src/main/java/mediatheque/CDPrinter.java
@@ -4,12 +4,12 @@ public class CDPrinter implements ItemVisitor {
 
     @Override
     public void visit(Book i) {
-        System.out.println(i.toString());
+        
     }
 
     @Override
     public void visit(CD i) {
-       i.toString();
+        System.out.println(i.toString());
        
     }
     
